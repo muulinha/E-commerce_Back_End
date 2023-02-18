f661023fde5589373d0487d92ad1e2a3ff79cf33
muulinha <lucasfl.araujo@hotmail.com>
2023-02-18 11:49:15 +0800

    Models completed

--- Develop/models/Category.js
+++ Develop/models/Category.js
@@ -6,7 +6,16 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+  }
   },
   {
     sequelize,
@@ -15,6 +24,7 @@ Category.init(
     underscored: true,
     modelName: 'category',
   }
+
 );
 
 module.exports = Category;
